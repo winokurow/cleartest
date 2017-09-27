3de2f11aa41510da753198f8db2aa3792a952839
winokurow <urodina@tut.by>
2017-09-27 12:11:09 +0200

    Aktueller Stand

--- src/main/java/de/clearit/test/framework/BasisPropertyManager.java
+++ src/main/java/de/clearit/test/framework/BasisPropertyManager.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
@@ -37,30 +38,35 @@ public class BasisPropertyManager {
 		loadPropertyFile("/localprop.properties");
 
 		final String dataset = System.getProperty("dataset", "dataset1");
+		String propertiesExtension = "properties";
 
 		String folderString2 = "/testdata/default";
-		File folder2 = new File(getClass().getResource(folderString2).getFile());
+		URL url = getClass().getResource(folderString2);
+		if (url != null) {
+			File folder2 = new File(getClass().getResource(folderString2).getFile());
+			File[] files2 = folder2.listFiles();
+			if (files2 != null) {
+				for (final File fileEntry : files2) {
 
-		String propertiesExtension = "properties";
-		File[] files2 = folder2.listFiles();
-		if (files2 != null) {
-			for (final File fileEntry : files2) {
+					if (fileEntry.getName().contains(propertiesExtension)) {
+						loadPropertyFile(fileEntry);
+					}
 
-				if (fileEntry.getName().contains(propertiesExtension)) {
-					loadPropertyFile(fileEntry);
 				}
-
 			}
 		}
 
 		String folderString1 = "/testdata/" + dataset;
-		File folder1 = new File(getClass().getResource(folderString1).getFile());
-		if (folder1 != null) {
-			File[] files1 = folder1.listFiles();
-			if (files1 != null) {
-				for (final File fileEntry : files1) {
-					if (fileEntry.getName().contains(propertiesExtension)) {
-						loadPropertyFile(fileEntry);
+		url = getClass().getResource(folderString1);
+		if (url != null) {
+			File folder1 = new File(getClass().getResource(folderString1).getFile());
+			if (folder1 != null) {
+				File[] files1 = folder1.listFiles();
+				if (files1 != null) {
+					for (final File fileEntry : files1) {
+						if (fileEntry.getName().contains(propertiesExtension)) {
+							loadPropertyFile(fileEntry);
+						}
 					}
 				}
 			}
